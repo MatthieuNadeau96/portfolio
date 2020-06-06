9e5d09449198e0dd6f547c39c83c4ab5333f3943
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-06-06 17:29:56 -0400

    navigation bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as ReactBootStrap from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ReactBootStrap.Navbar bg="light" expand="lg" sticky="top">
+        <ReactBootStrap.Navbar.Brand href="#home">Matthieu Nadeau</ReactBootStrap.Navbar.Brand>
+        <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
+          <ReactBootStrap.Nav className="mr-auto">
+          </ReactBootStrap.Nav>
+          <ReactBootStrap.Nav className="">
+            <ReactBootStrap.Nav.Link className="mr-auto" href="#home">About</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#apps">Apps</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#resume">Resume</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#contact">Contact</ReactBootStrap.Nav.Link>
+          </ReactBootStrap.Nav>
+        </ReactBootStrap.Navbar.Collapse>
+      </ReactBootStrap.Navbar>
+      <h1>Hello</h1>
+    </div >
   );
 }
 
