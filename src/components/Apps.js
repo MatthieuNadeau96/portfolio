9014718ffbbef9e9c378e779a3a1eb722464dc7c
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-06-08 18:25:51 -0400

    added more styling to the apps screen

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -4,6 +4,9 @@ import { Grid, Paper } from '@material-ui/core';
 import ButtonBase from '@material-ui/core/ButtonBase';
 
 const useStyles = makeStyles((theme) => ({
+    div: {
+        // padding: '4em',
+    },
     grid: {
         width: '100%',
         alignItems: 'center',
@@ -18,11 +21,15 @@ const useStyles = makeStyles((theme) => ({
     image: {
         width: '100%',
         height: '100%',
+        outline: 'none !important',
+        backgroundColor: '#dddddd',
 
     },
     img: {
         margin: 'auto',
         display: 'block',
+        height: '50%',
+        width: '50%',
         maxWidth: '100%',
         maxHeight: '100%',
     },
@@ -32,39 +39,41 @@ function Apps() {
     const classes = useStyles();
 
     return (
-        <Grid container spacing={2} className={classes.grid} >
-            <Grid item xs={12} sm={12} md={6} lg={4}>
-                <ButtonBase className={classes.image}>
-                    <img className={classes.img} alt="complex" src="/images/pomodoro.png" />
-                </ButtonBase>
-            </Grid>
+        <div className={classes.div}>
+            <Grid container spacing={5} className={classes.grid} >
+                <Grid item xs={12} sm={12} md={6} lg={4}>
+                    <ButtonBase className={classes.image}>
+                        <img className={classes.img} alt="complex" src="/images/pomodoro.png" />
+                    </ButtonBase>
+                </Grid>
 
-            <Grid item xs={12} sm={12} md={6} lg={4}>
-                <ButtonBase className={classes.image}>
-                    <img className={classes.img} alt="complex" src="/images/chat-ui.png" />
-                </ButtonBase>
+                <Grid item xs={12} sm={12} md={6} lg={4}>
+                    <ButtonBase className={classes.image}>
+                        <img className={classes.img} alt="complex" src="/images/chat-ui.png" />
+                    </ButtonBase>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} lg={4}>
+                    <ButtonBase className={classes.image}>
+                        <img className={classes.img} alt="complex" src="/images/personal-expenses.png" />
+                    </ButtonBase>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} lg={4}>
+                    <ButtonBase className={classes.image}>
+                        <img className={classes.img} alt="complex" src="/images/snake.png" />
+                    </ButtonBase>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} lg={4}>
+                    <ButtonBase className={classes.image}>
+                        <img className={classes.img} alt="complex" src="/images/meals.png" />
+                    </ButtonBase>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} lg={4}>
+                    <ButtonBase className={classes.image}>
+                        <img className={classes.img} alt="complex" src="/images/xylophone.png" />
+                    </ButtonBase>
+                </Grid>
             </Grid>
-            <Grid item xs={12} sm={12} md={6} lg={4}>
-                <ButtonBase className={classes.image}>
-                    <img className={classes.img} alt="complex" src="/images/personal-expenses.png" />
-                </ButtonBase>
-            </Grid>
-            <Grid item xs={12} sm={12} md={6} lg={4}>
-                <ButtonBase className={classes.image}>
-                    <img className={classes.img} alt="complex" src="/images/snake.png" />
-                </ButtonBase>
-            </Grid>
-            <Grid item xs={12} sm={12} md={6} lg={4}>
-                <ButtonBase className={classes.image}>
-                    <img className={classes.img} alt="complex" src="/images/meals.png" />
-                </ButtonBase>
-            </Grid>
-            <Grid item xs={12} sm={12} md={6} lg={4}>
-                <ButtonBase className={classes.image}>
-                    <img className={classes.img} alt="complex" src="/images/xylophone.png" />
-                </ButtonBase>
-            </Grid>
-        </Grid>
+        </div>
     );
 }
 
