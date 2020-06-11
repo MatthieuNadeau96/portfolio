3afc17da744038ed83aa07fa89a600c3f396af54
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-06-11 14:25:47 -0400

    Updates

---
+++ precache-manifest.b222c46a157dfa5bccd0f68de2833946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de17121872e2d307c070506eb6bb490",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "112902633f4e1457e6a6",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "639bd3972dd7b6e63f11",
+    "url": "/portfolio/static/css/main.bea9e97a.chunk.css"
+  },
+  {
+    "revision": "112902633f4e1457e6a6",
+    "url": "/portfolio/static/js/2.38387821.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio/static/js/2.38387821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "639bd3972dd7b6e63f11",
+    "url": "/portfolio/static/js/main.f88e3bf1.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
